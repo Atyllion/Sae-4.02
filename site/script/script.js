588a1663af940805dbca8d84974977968071d9cb
Ethan Manchon <manchon.ethan@gmail.com>
2025-02-27 08:42:20 +0100

    Add files via upload

--- site/script/script.js
+++ site/script/script.js
@@ -5,33 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     container.addEventListener('wheel', (event) => {
         event.preventDefault(); // Empêche le défilement vertical par défaut
         container.scrollBy({
-            left: event.deltaY * 2,
+            left: event.deltaY * (window.innerWidth / 100),
             behavior: 'smooth'
         });
     });
 });
-
-
-gsap.registerPlugin(ScrollTrigger);
-
-    gsap.to('.find-text', {
-        scrollTrigger: {
-            trigger: '.apple',
-            start: 'top 50%',
-            end: '+=300',
-            onEnter: () => {
-                // Désactivez le défilement lorsque l'animation commence
-                container.style.overflow = 'hidden';
-            },
-            onLeaveBack: () => {
-                // Réactivez le défilement lorsque l'animation est terminée
-                container.style.overflow = '';
-            },
-            onLeave: () => {
-                // Réactivez le défilement lorsque l'animation est terminée
-                container.style.overflow = '';
-            }
-        },
-        opacity: 1,
-        duration: 10,
-    });
